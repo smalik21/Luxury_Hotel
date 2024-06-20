9cad825e6010ff7c315f06fccaf1fcb0d5f0a6bc
krishnas05 <kanavkrishna05@gmail.com>
2024-06-20 09:56:53 +0530

    ui updates

--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,21 @@ import React from 'react';
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import Features from '@/components/FeatureSection';
-
 import Map from '../components/Map'
 import Experts from '../components/Experts'
 import BestChoice from '../components/BestChoice'
 
 const Home = () => {
   return (
-<<<<<<< HEAD
     <div>
-      <Header/>
-      <HeroSection/>
+      <Header />
+      <HeroSection />
       <Features />
+      <Map />
+      <BestChoice />
+      <Experts />
+
     </div>
-=======
-       <div>
-           
-           <Map/>
-           <BestChoice/>
-          <Experts/>
-         
-       </div>
->>>>>>> 923ec61a8d3e8808dab85fb1945a250ad82d561d
   )
 }
 
--- src/components/FeatureSection.js
+++ src/components/FeatureSection.js
@@ -13,30 +13,30 @@ const Features = () => {
                         </div>
                         <div className="order-2 md:order-1 md:w-1/2">
                             <div className="max-w-md mx-auto md:mx-0">
-                                <h3 className="text-xl text-[36px] font-semibold mb-2 text-black">Impressive experience</h3>
-                                <p className="text-gray-600 text-[21px] max-w-[370px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <h3 className="text-[36px] font-semibold mb-2 text-black">Impressive experience</h3>
+                                <p className="text-gray-600 text-[18px] max-w-[370px] leading-tight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-col md:flex-row items-center md:space-x-2 ">
+                    <div className="flex flex-col md:flex-row items-center md:space-x-2 pb-[120px]">
                         <div className="order-1 md:w-1/2">
                             <div className="h-48 bg-gradient-to-t to-gray-400 from-white mx-auto md:mx-0 rounded-md" style={{ maxWidth: '405.2px', maxHeight: '233px' }}></div>
                         </div>
                         <div className="order-2 md:w-1/2">
                             <div className="max-w-md mx-auto md:mx-0">
-                                <h3 className="text-xl text-[36px] font-semibold mb-2 text-black">Premium sport destination</h3>
-                                <p className="text-gray-600 text-[21px] max-w-[370px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <h3 className="text-[36px] font-semibold mb-2 text-black leading-tight">Premium sport destination</h3>
+                                <p className="text-gray-600 text-[18px] max-w-[370px] leading-tight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-col md:flex-row items-center md:space-x-2 mt-[120px]">
+                    <div className="flex flex-col md:flex-row items-center md:space-x-2 ">
                         <div className="order-1 md:order-2 md:w-1/2">
                             <div className="h-48 bg-gradient-to-t to-gray-400 from-white mx-auto md:mx-0 rounded-md" style={{ maxWidth: '405.2px', maxHeight: '233px' }}></div>
                         </div>
                         <div className="order-2 md:order-1 md:w-1/2">
                             <div className="max-w-md mx-auto md:mx-0">
-                                <h3 className="text-xl text-[36px] font-semibold mb-2 text-black">Big savings</h3>
-                                <p className="text-gray-600 text-[21px] max-w-[370px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <h3 className="text-[36px] font-semibold mb-2 text-black">Big savings</h3>
+                                <p className="text-gray-600 text-[18px] max-w-[370px] leading-tight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
                     </div>
