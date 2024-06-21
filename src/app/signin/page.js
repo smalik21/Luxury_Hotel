77bad147fd456217369ee36fa6e3d0572e0d786b
pr19gya <pragya.shri2001@gmail.com>
2024-06-21 18:31:26 +0530

    sign in update

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,54 +1,75 @@
-import React from 'react'
+"use client";
+import React, {useState} from 'react'
 
 
 const page = () => {
-    return (
-        <>
-            <div className='flex h-screen w-screen'>
-                <div className='w-2/5 '>
-                    <div className='h-screen flex items-center justify-center mx-[4rem]'>
-                        <div>
-                            <div className='text-center text-3xl mb-3'>LuxuryHotelConcierge</div>
-                            <div className='text-center text-gray-400 text-md'>Discover the epitome of luxury and comfort at our world-renowned hotels..</div>
-                            <div className='text-center text-3xl my-5'>SignIn</div>
-                            <div className='mx-10'>
-                                <div className="relative mb-4 justify-center items-center">
-                                    <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                                            <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
-                                            <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
-                                        </svg>
-                                    </div>
-                                    <input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-[25rem] ps-10 p-2.5 " placeholder="Enter your email" />
-                                </div>
+    const [isHiddenDivVisible, setIsHiddenDivVisible] = useState(false);
 
-                            </div>
-
-
-                            <button type="button" className="text-white w-[25rem] ml-10 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Continue</button>
-                            <div className='text-gray-700 text-sm mb-2 ml-[11.2rem]'>Forgot password?</div>
-
-                            <div className="relative flex py-2 items-center">
-                                <div className="flex-grow border-t border-gray-400"></div>
-                                <span className="flex-shrink mx-4 text-gray-400">or</span>
-                                <div className="flex-grow border-t border-gray-400"></div>
-                            </div>
-                            <div className='text-gray-700 text-sm mb-2 ml-[12.8rem]'>Signin with</div>
-                            <button type="button" className="text-white w-[25rem] ml-10 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Google</button>
-
-                        </div>
-                    </div>
-
-                    {/* <div className=' mb-0'>By signing up, you agree to our Terms of Service & Privacy policy</div> */}
-
+    const toggleHiddenDiv = () => {
+        setIsHiddenDivVisible(!isHiddenDivVisible);
+    };
+  return (
+    <>
+    <div className='m-auto'>
+    {isHiddenDivVisible && (
+                <div>
+                    <div onClick={toggleHiddenDiv} className='m-auto absolute w-[4rem] h-[4rem] bg-white rounded-full mt-[7rem] ml-[48rem] p-2'></div>
+                <div className=' absolute h-[13rem] w-[30rem] rounded-3xl bg-white ml-[35rem] mt-[15rem] border-2 border-black'>
+                               <input type="text" id="input-group-1" className=" m-auto mt-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-[25rem] ps-10 p-2.5 " placeholder="Enter your email"/>
+                               <button type="button" className="mt-5 text-white w-[25rem] ml-10 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Continue</button>
+                                <div className=' absolute mt-2 ml-[12rem] '>Back to login</div>
                 </div>
+                <div className='absolute bg-white rounded-3xl h-[3rem] w-[15rem] ml-[80rem] mt-[5rem] pl-7 pt-3'>Reset link to your email.   </div>
+                </div>
+            )}
+    </div>
+    <div className={isHiddenDivVisible ? 'flex h-screen w-screen ' : 'flex h-screen w-screen'}>
+    
+        <div className='w-2/5 '>
+        <div className='h-screen flex items-center justify-center mx-[4rem]'>
+            <div>
+            <div className='text-center text-3xl mb-3'>LuxuryHotelConcierge</div>
+            <div className='text-center text-gray-400 text-md'>Discover the epitome of luxury and comfort at our world-renowned hotels..</div>
+            <div className='text-center text-3xl my-5'>Sign In</div>
+           <div className='mx-10'>
+            <div className="relative mb-4 justify-center items-center">
+            <div className=" inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                {/* <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                    <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
+                    <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
+                </svg> */}
+            </div>
+            <input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-[25rem] ps-10 p-2.5 " placeholder="Enter your email"/>
+            </div>
+            
+            </div>
+            
+   
+            <button type="button" className="text-white w-[25rem] ml-10 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Continue</button>
+            <div onClick={toggleHiddenDiv} className='text-gray-700 text-sm mb-2 ml-[11.2rem]'>Forgot password?</div>
 
-                <img className='object-fill w-3/5' src="/sign.png" />
 
+            <div className="relative flex py-2 items-center">
+                <div className="flex-grow border-t border-gray-400"></div>
+                <span className="flex-shrink mx-4 text-gray-400">or</span>
+                <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+            <div className='text-gray-700 text-sm mb-2 ml-[12.8rem]'>Signin with</div>
+            <button type="button" className="text-white w-[25rem] ml-10 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Google</button>
 
             </div>
-        </>
-    )
+        </div>
+
+        {/* <div className=' mb-0'>By signing up, you agree to our Terms of Service & Privacy policy</div> */}
+        
+        </div>
+       
+            <img className='object-fill w-3/5' src="/sign.png"/>
+       
+        
+    </div>
+    </>
+  )
 }
 
 export default page
--- src/components/home/Reviews.jsx
+++ src/components/home/Reviews.jsx
@@ -4,28 +4,28 @@ import React from 'react'
 const Reviews = () => {
   return (
     <>
-    <div className='h-[812px]'>
+    <div className='h-[780px] m-0'>
         <div className='m-[3rem]'>
-        <div className='text-center text-7xl'> <b>Our Satisfied Clients</b></div>
+        <div className='text-center text-5xl'> <b>Our Satisfied Clients</b></div>
         <div className='text-center mt-5 text-[#808080] text-2xl'>we are dedicated to providing an unparalleled experience for our guests.</div>
         </div>
         <div className='flex mx-10 my-[3rem] justify-center items-center'>
             <div className='w-1/2 ml-[10rem] mt-5'  >
-            <div className='h-[7rem] w-[25rem] flex bg-gray-200  mb-10 rounded-full hover:border-black hover:border-2'>
-                <div className='p-7 rounded-full w-[5rem] h-[5rem] ml-5 mt-4 bg-black'></div>
+            <div className='h-[6.5rem] w-[25rem] flex bg-gray-200  mb-10 rounded-full hover:border-black hover:border-2'>
+                <div className='p-7 rounded-full w-[5rem] h-[5rem] ml-5 mt-3 bg-black'></div>
                 <div className='ml-10 text-xl mt-7'><b>John Dey</b></div>
             </div>
-            <div className='h-[7rem] w-[25rem] flex bg-gray-200 mb-10 rounded-full hover:border-black hover:border-2'>
-            <div className='p-7 rounded-full w-[5rem] h-[5rem] ml-5 mt-4 bg-black'></div>
+            <div className='h-[6.5rem] w-[25rem] flex bg-gray-200 mb-10 rounded-full hover:border-black hover:border-2'>
+            <div className='p-7 rounded-full w-[5rem] h-[5rem] ml-5 mt-3 bg-black'></div>
             <div className='ml-10 text-xl mt-7'><b>John Dey</b></div>
             </div>
-            <div className='h-[7rem] w-[25rem] flex bg-gray-200  mb-10 rounded-full hover:border-black hover:border-2'>
-            <div className='p-7 rounded-full w-[5rem] h-[5rem] ml-5 mt-4 bg-black'></div>
+            <div className='h-[6.5rem] w-[25rem] flex bg-gray-200  mb-10 rounded-full hover:border-black hover:border-2'>
+            <div className='p-7 rounded-full w-[5rem] h-[5rem] ml-5 mt-3 bg-black'></div>
             <div className='ml-10 text-xl mt-7'><b>John Dey</b></div>
             </div>
             </div>
             <div className='w-1/2  '>
-                <div className='text-3xl mb-[2rem] '><b>Absolute good service, satisfied</b></div>
+                <div className='text-2xl mb-[2rem] '><b>Absolute good service, satisfied</b></div>
                 <div className='text-[#808080] text-xl leading-normal mr-[3rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
             </div>
         </div>
