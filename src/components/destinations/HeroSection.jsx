1926b014f3184984c19d0cea4654d5ef09140ca8
Krishna Sharma <kanavkrishna05@gmail.com>
2024-06-23 21:04:29 +0530

    ui

--- src/components/destinations/HeroSection.jsx
+++ src/components/destinations/HeroSection.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                 <div className="absolute inset-0 bg-black opacity-30"></div>
             </div>
             <div className="relative flex flex-col items-center justify-center h-full text-center text-white pointer-events-auto">
-                <h1 className="md:text-[70px] text-[60px] font-bold mb-8 mt-[130px]">Destinations</h1>
+                <h1 className="md:text-[70px] text-[40px] font-bold mb-8 mt-[130px]">Destinations</h1>
                 <p className="text-lg md:text-[19px] text-[14px]">explore new destinations..</p>
                 <p>Home › explore</p>
             </div>
