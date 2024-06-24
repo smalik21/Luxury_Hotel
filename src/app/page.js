9f8efe9fc6bc5f95d5b746e8db9b9bde1b8a67fa
janvi jindal <janvijindal05@gmail.com>
2024-06-24 07:56:09 +0530

    folder added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import LandingPage from '@/components/LandingPage';
+import Header from '@/components/Header';
+import HeroSection from '@/components/home/HeroSection';
+import Features from '@/components/home/FeatureSection';
+import Map from '@/components/home/Map'
+//import Experts from '@/components/home/Experts'
+import BestChoice from '@/components/home/BestChoice'
+//import Reviews from '@/components/home/Reviews'
+import Footer from '@/components/Footer'
 
 const Home = () => {
-  return (
-    <div>
-      <LandingPage />
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <HeroSection />
+            <Features />
+            <Map />
+            <BestChoice />
+            <Footer />
+        </div>
+    )
 }
 
 export default Home;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <header className="flex justify-between items-center p-6 absolute w-full top-0 z-10 bg-opacity-50 px-8 mt-4">
             <Link href="/home" className="lg:text-2xl text-lg font-bold text-white text-[24px]">LuxuryHotelConcierge</Link>
             <nav className="hidden lg:flex flex-grow justify-center space-x-4 text-[16px] gap-5">
-                <Link href="/home" className={`text-white ${pathname === '/home' ? 'font-bold' : ''}`}>Home</Link>
+                <Link href="/" className={`text-white ${pathname === '/' ? 'font-bold' : ''}`}>Home</Link>
                 <Link href="/explore" className={`text-white ${pathname === '/explore' ? 'font-bold' : ''}`}>Explore</Link>
                 <Link href="/destinations" className={`text-white ${pathname === '/destinations' ? 'font-bold' : ''}`}>Destinations</Link>
                 <Link href="/blog" className={`text-white ${pathname === '/blog' ? 'font-bold' : ''}`}>Blog</Link>
