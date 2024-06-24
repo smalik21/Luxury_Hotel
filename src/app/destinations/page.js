951cafc84ed1ebc92db9116672f8660905173716
Krishna Sharma <kanavkrishna05@gmail.com>
2024-06-24 11:41:00 +0530

    destination page

--- src/app/destinations/page.js
+++ src/app/destinations/page.js
@@ -3,6 +3,10 @@ import Header from '@/components/Header';
 import Hero from '@/components/destinations/HeroSection';
 import FeaturedDestinations from '@/components/destinations/FeaturedDestination';
 import BestOfOne from '@/components/destinations/BestOfOne';
+import Footer from '@/components/Footer';
+import hotels from "@/data/BestOfOne.json"
+import StaycationDeals from '@/components/destinations/StaycationDeals';
+import WhyChooseUs from '@/components/destinations/WhyChooseUs';
 
 const page = () => {
     return (
@@ -10,7 +14,11 @@ const page = () => {
             <Header />
             <Hero />
             <FeaturedDestinations />
-            <BestOfOne/>
+            <BestOfOne city="New York" hotels={hotels}/>
+            <WhyChooseUs />
+            <StaycationDeals />
+            <BestOfOne city="Singapore" hotels={hotels}/>
+            <Footer />
         </div>
     );
 }
