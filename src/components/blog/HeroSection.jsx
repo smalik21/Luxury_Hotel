f07d8b9e8ad238799f9579433835599e267a2e2b
Krishna Sharma <kanavkrishna05@gmail.com>
2024-06-27 21:47:02 +0530

    animations

--- src/components/blog/HeroSection.jsx
+++ src/components/blog/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Image from 'next/image';
-// import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
     return (
@@ -15,7 +17,18 @@ const Hero = () => {
                 <div className="absolute inset-0 bg-black opacity-30"></div>
             </div>
             <div className="relative flex flex-col items-center justify-center h-full text-center text-white pointer-events-auto">
-                <h1 className="md:text-[70px] text-[60px] font-bold mb-8 mt-[130px]">Blogs</h1>
+            <h1 className="md:text-[70px] text-[60px] font-bold mb-8 mt-[130px]"><TypeAnimation
+                sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    'Blogs',
+                    5000 // wait 1s before replacing "Mice" with "Hamsters"
+
+                ]}
+                wrapper="span"
+                speed={50}
+                // style={{ display: 'inline-block' }}
+                repeat={Infinity}
+            /></h1>
                 <p className="text-lg md:text-[19px] text-[14px]">Insightful travel & Hotel booking advice.</p>
                 <p>Home › blogs</p>
             </div>
--- src/components/explore/HeroSection.jsx
+++ src/components/explore/HeroSection.jsx
@@ -1,6 +1,7 @@
-"use client"
+"use client";
+
 import Image from 'next/image';
-// import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
     // let text= new SplitType("#text");
@@ -19,7 +20,18 @@ const Hero = () => {
             </div>
             <div className="relative flex flex-col items-center justify-center h-full text-center text-white pointer-events-auto">
             
-                <h1  id="text" className="md:text-[70px] text-[60px] font-bold mb-8 mt-[130px]">Explore</h1>
+            <h1  id="text" className="md:text-[70px] text-[60px] font-bold mb-8 mt-[130px]"><TypeAnimation
+                sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    'Explore',
+                    5000 // wait 1s before replacing "Mice" with "Hamsters"
+
+                ]}
+                wrapper="span"
+                speed={50}
+                // style={{ display: 'inline-block' }}
+                repeat={Infinity}
+            /></h1>
                 <p className="text-lg md:text-[19px] text-[14px]">Explore new destinations..</p>
                 <p>Home › explore</p>
             </div>
