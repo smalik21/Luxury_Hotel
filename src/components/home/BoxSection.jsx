d5bb71dcf19692a93c3c55538af54ef311be8ba2
Krishna Sharma <kanavkrishna05@gmail.com>
2024-06-27 16:54:59 +0530

    hero section

--- src/components/home/BoxSection.jsx
+++ src/components/home/BoxSection.jsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { Carousel } from 'react-responsive-carousel';
 import DatePicker from 'react-datepicker';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { FaSearch } from 'react-icons/fa';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import Link from 'next/link';
 import { IoIosArrowForward } from "react-icons/io";
@@ -22,8 +20,7 @@ const BoxSection = () => {
   const [checkOutOpen, setCheckOutOpen] = useState(false);
   const [tab, setTab] = useState('hotels');
   const [location, setLocation] = useState('')
-  const router = useRouter()
-  
+  const router = useRouter();
 
   const api = base_url;
 
@@ -63,10 +60,9 @@ const BoxSection = () => {
     }
   }, [selectedCountry]);
 
-
   const handleCountryChange = (e) => {
     setSelectedCountry(e.target.value);
-    setSelectedCity(''); // Reset city selection
+    setSelectedCity('');
   };
 
   const handleCityChange = (e) => {
@@ -74,8 +70,8 @@ const BoxSection = () => {
   };
 
   const handleSearch = () => {
-    console.log("selected:", selectedCity)
-    if (!selectedCity) return
+    console.log("selected:", selectedCity);
+    if (!selectedCity) return;
     router.push(`/hotels?search=${selectedCity}`);
   };
 
@@ -100,33 +96,7 @@ const BoxSection = () => {
 
   return (
     <div className="relative bg-white h-[400px] sm:h-[500px] w-auto top-0 left-0 right-0">
-      {/* <Carousel
-        showThumbs={false}
-        autoPlay
-        interval={3000}
-        infiniteLoop
-        showStatus={false}
-        showArrows={false}
-        showIndicators={false}
-        className="h-[790px]"
-        emulateTouch
-      >
-        {/* <div className="h-[670px] sm:h-[700px]">
-          <img src="/hero-image.jpeg" alt="Background 1" className="h-[790px] object-cover" />
-        </div>
-        <div className="h-[670px] sm:h-[700px]">
-          <img src="/luxury-hotel-3.webp" alt="Background 2" className="h-[790px] object-cover" />
-        </div>
-        <div className="h-[670px] sm:h-[700px]">
-          <img src="/luxury-hotel-2.webp" alt="Background 3" className="h-[790px] object-cover" />
-        </div>
-        <div className="h-[670px] sm:h-[700px]">
-          <img src="/luxury-hotel-4.jpg" alt="Background 4" className="h-[790px] object-cover" />
-        </div> */}
-      {/* </Carousel> */}
-
-      <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center text-center md:pt-0 pt-[60px] text-black bg-white  px-4">
-        {/* <p className="border border-gray-300 py-1 px-3 text-[14px] rounded-2xl md:mt-[140px] text-gray-200">it's time for vacation 🚀</p> */}
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center text-center md:pt-0 pt-[60px] text-black bg-white px-4">
         <h2 className="md:text-[50px] text-[40px] font-bold">Discover Luxury</h2>
         <p className="text-[15px] text-black">Experience the ultimate in hospitality..</p>
 
@@ -163,15 +133,7 @@ const BoxSection = () => {
             <>
               <div className="flex items-center mb-4 w-full">
                 <div className="flex justify-between city-search px-2 gap-4 w-full">
-                  {/* <input
-                                        type="text"
-                                        value={searchText}
-                                        onChange={handleSearchChange}
-                                        placeholder="Search for a city..."
-                                        className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none"
-                                    /> */}
                   <select className=" px-4 py-2 bg-gray-200 rounded-lg focus:outline-none w-1/2" value={selectedCountry} onChange={handleCountryChange}>
-
                     <option className='sm:text-xs' value="">Select Country</option>
                     {countries.map((country) => (
                       <option key={country.country} value={country.country}>
@@ -188,19 +150,6 @@ const BoxSection = () => {
                       </option>
                     ))}
                   </select>
-                  {/* {cities && cities.length > 0 && (
-                                        <div className="absolute z-50 bg-white mt-1 w-full border border-gray-300 rounded-lg">
-                                            {cities.map((city) => (
-                                                <div
-                                                    key={city.id}
-                                                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                                                    onClick={() => handleCitySelect(city.name)}
-                                                >
-                                                    {city.name}
-                                                </div>
-                                            ))}
-                                        </div>
-                                    )} */}
                 </div>
               </div>
               <div className="flex items-center space-x-2">
@@ -296,127 +245,25 @@ const BoxSection = () => {
           )}
 
           {tab === 'visas' && (
-            <>
-              <div className="flex items-center mb-4 ">
-                <div className="relative location-search px-2 flex-grow">
-                  <input
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Country"
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none"
-                  />
-                </div>
-                {/* <div className="relative location-search flex-grow">
-                  <input
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Destination"
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none"
-                  />
-                </div> */}
-              </div>
-              <div className="flex items-center space-x-2">
-                <div className="relative px-2 check-in-date flex-grow">
-                  <button
-                    onClick={() => setCheckInOpen(!checkInOpen)}
-                    className="px-4 py-2 bg-gray-200 rounded-lg w-full text-left"
-                  >
-                    {checkInDate ? `${checkInDate.toLocaleDateString()}` : 'Select Date'}
-                  </button>
-                  {checkInOpen && (
-                    <div className="absolute z-50">
-                      <DatePicker
-                        selected={checkInDate}
-                        onChange={(date) => {
-                          setCheckInDate(date);
-                          setCheckInOpen(false);
-                        }}
-                        inline
-                        className="text-sm"
-                      />
-                    </div>
-                  )}
-                  
-
-                  
-                </div>
-               
-                <button
-                  className="bg-black text-white px-4 py-2 rounded-3xl flex items-center justify-center"
-                >
-                  <FaSearch className="mr-2" />
-                  Search
-                </button>
-              </div>
-            </>
+            <div className="flex flex-col items-center">
+              <Link href="/visas" className="bg-black text-white px-4 py-2 rounded-3xl flex items-center justify-center">
+                Learn more <IoIosArrowForward className="ml-2" />
+              </Link>
+            </div>
           )}
+
           {tab === 'services' && (
-            <>
-              <div className="flex items-center mb-4 ">
-                <div className="relative location-search px-2 flex-grow">
-                  <input
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Boarding"
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none"
-                  />
-                </div>
-                <div className="relative location-search flex-grow">
-                  <input
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Destination"
-                    className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none"
-                  />
-                </div>
-              </div>
-              <div className="flex items-center space-x-2">
-                <div className="relative px-2 check-in-date flex-grow">
-                  <button
-                    onClick={() => setCheckInOpen(!checkInOpen)}
-                    className="px-4 py-2 bg-gray-200 rounded-lg w-full text-left"
-                  >
-                    {checkInDate ? `${checkInDate.toLocaleDateString()}` : 'Select Date'}
-                  </button>
-                  {checkInOpen && (
-                    <div className="absolute z-50">
-                      <DatePicker
-                        selected={checkInDate}
-                        onChange={(date) => {
-                          setCheckInDate(date);
-                          setCheckInOpen(false);
-                        }}
-                        inline
-                        className="text-sm"
-                      />
-                    </div>
-                  )}
-                  
-
-                  
-                </div>
-               
-                <button
-                  className="bg-black text-white px-4 py-2 rounded-3xl flex items-center justify-center"
-                >
-                  <FaSearch className="mr-2" />
-                  Search
-                </button>
-              </div>
-            </>
+            <div className="flex flex-col items-center">
+              <Link href="/procurement-services" className="bg-black text-white px-4 py-2 rounded-3xl flex items-center justify-center">
+                Learn more <IoIosArrowForward className="ml-2" />
+              </Link>
+            </div>
           )}
+
         </div>
-        <Link href="/explore" className="sm:bottom-12 bottom-9 mt-0 absolute bg-black  border-white text-white rounded-3xl px-4 py-2 flex items-center gap-2 font-bold">
-          Explore <div className="ml-1 text-md font-extrabold"><IoIosArrowForward /></div>
-        </Link>
       </div>
     </div>
   );
 };
 
 export default BoxSection;
-
--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,61 +1,83 @@
-"use client"
-import { useEffect } from 'react';
-import { Carousel } from 'react-responsive-carousel';
+"use client";
+
+import { useEffect, useState } from 'react';
+import { useTransition, animated } from '@react-spring/web';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+const images = [
+  { src: '/hero-image.jpeg', alt: 'Background 1', title: 'The Carlton', description: 'Experience the Royal Era with our selection of Palaces!' },
+  { src: '/luxury-hotel-3.webp', alt: 'Background 2', title: 'Aman Le melezin', description: 'Let us Take you on your next vacation with your loved ones!' },
+  { src: '/luxury-hotel-2.webp', alt: 'Background 3', title: 'Umaid Bhawan Palace', description: 'Experience the Royal Era with our selection of Palaces!' },
+  { src: '/luxury-hotel-4.jpg', alt: 'Background 4', title: 'The Adare Manor', description: 'The Rich History held within the Castles of Ireland awaits you!' },
+];
+
 const Hero = () => {
+  const [index, setIndex] = useState(0);
+
+  const imageTransitions = useTransition(index, {
+    keys: index,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 1000 },
+  });
+
+  const textTransitions = useTransition(index, {
+    keys: index,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 1000 },
+  });
+
   useEffect(() => {
-    // Ensure Carousel is initialized after component mounts on the client
-    import('react-responsive-carousel').then((module) => {
-      const { Carousel } = module;
-      if (Carousel) {
-        // Initialize Carousel here if needed
-      }
-    });
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
   }, []);
 
-  const renderIndicator = (onClickHandler, isSelected, index, label) => {
-    const indicatorClass = isSelected ? "bg-blue-500" : "bg-gray-300";
+  const renderDots = () => {
     return (
-      <li
-        key={index}
-        className={`inline-block w-3 h-3 rounded-full ${indicatorClass} mx-1 cursor-pointer`}
-        onClick={onClickHandler}
-        onKeyDown={onClickHandler}
-        value={index}
-        role="button"
-        tabIndex={0}
-        aria-label={`${label} ${index + 1}`}
-      />
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-4">
+        {images.map((_, i) => (
+          <span
+            key={i}
+            className={`block w-3 h-3 rounded-full border-2 border-white ${index === i ? 'bg-white' : ''} cursor-pointer`}
+            onClick={() => setIndex(i)}
+          />
+        ))}
+      </div>
     );
   };
 
   return (
-    <div className="relative h-screen w-full">
-      <Carousel
-        showThumbs={false}
-        autoPlay
-        interval={3000}
-        infiniteLoop
-        showStatus={false}
-        showArrows={false}
-        renderIndicator={renderIndicator}
-        emulateTouch
-        className="h-full"
-      >
-        <div className="h-full">
-          <img src="/hero-image.jpeg" alt="Background 1" className="h-full w-full object-cover" />
-        </div>
-        <div className="h-full">
-          <img src="/luxury-hotel-3.webp" alt="Background 2" className="h-full w-full object-cover" />
-        </div>
-        <div className="h-full">
-          <img src="/luxury-hotel-2.webp" alt="Background 3" className="h-full w-full object-cover" />
-        </div>
-        <div className="h-full">
-          <img src="/luxury-hotel-4.jpg" alt="Background 4" className="h-full w-full object-cover" />
-        </div>
-      </Carousel>
+    <div className="relative h-[650px] w-full overflow-hidden">
+      {imageTransitions((style, i) => (
+        <animated.div key={i} className="absolute w-full h-full" style={style}>
+          <div className="relative h-full">
+            <img src={images[i].src} alt={images[i].alt} className="h-full w-full object-cover" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center md:pl-[120px] text-white">
+              <div className=" max-w-lg md:max-w-2xl p-4 md:p-8">
+                {textTransitions((textStyle, tIndex) => (
+                  tIndex === i && (
+                    <animated.div style={textStyle}>
+                      <h2 className="text-3xl md:text-5xl font-f_1 font-semibold">
+                        {images[tIndex].title}
+                      </h2>
+                      <p className="mt-2 text-md md:text-lg font-f_2">
+                        {images[tIndex].description}
+                      </p>
+                    </animated.div>
+                  )
+                ))}
+              </div>
+            </div>
+          </div>
+        </animated.div>
+      ))}
+      {renderDots()}
     </div>
   );
 };
